7acccd109872e1b83ccc4a18bd2caf1d8c53cb1f
Nikita Bobko <nikitabobko@gmail.com>
2016-10-20 17:28:43 +0300

    Solve 03/02.c

--- 03/02.c
+++ 03/02.c
@@ -2,8 +2,9 @@
 
 int isPrime(int n) {
 	if (n == 1) return 0;
+	if (n % 2 == 0) return (n == 2);
 	int i;
-	for (i = 2; i < n; i++) {
+	for (i = 3; i*i <= n; i++) {
 		if (n % i == 0) return 0;
 	}
 	return 1;
