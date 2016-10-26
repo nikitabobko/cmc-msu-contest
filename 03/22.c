09f1ec3bbac863f5056c9d9db8f68ba9ccbe400a
Nikita Bobko <nikitabobko+git@gmail.com>
2016-10-26 21:08:18 +0300

    Solve 03/22.c

--- 03/22.c
+++ 03/22.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addOneToDigit(char** arr, int digitIndex) {
+void addOneToDigit(char** arr, int digitIndex, int* countOfNumbers) {
+	if (digitIndex == -1) {
+		int i;
+		for (i = *countOfNumbers; i >= 1; i--) {
+			(*arr)[i] = (*arr)[i-1];
+		}
+		(*arr)[0] = 0;
+		digitIndex = 0;
+		(*countOfNumbers)++;
+	}
 	(*arr)[digitIndex]++;
 	if ((*arr)[digitIndex] == 10) {
 		(*arr)[digitIndex] = 0;
-		addOneToDigit(arr, digitIndex-1); 
-		return 1;
+		addOneToDigit(arr, digitIndex-1, countOfNumbers);
 	}
-	return 0;
 }
 
-void transform(int sum1, int sum2, int reSumSum1AndSum2, char** string, int countOfNumbers) {
-	int i;
-	if (reSumSum1AndSum2) {
-		sum1 = 0;
-		sum2 = 0;
-		for (i = 0; i < countOfNumbers / 2; i++) {
-			sum1 += (*string)[i];
-		}
-		for (i = countOfNumbers / 2; i < countOfNumbers; i++) {
-			sum2 += (*string)[i];
-		}
+void transform(char** string, int* countOfNumbers) {
+	int i, sum1 = 0, sum2 = 0;
+	for (i = 0; i < (*countOfNumbers) / 2; i++) {
+		sum1 += (*string)[i];
+	}
+	for (i = (*countOfNumbers) / 2; i < (*countOfNumbers); i++) {
+		sum2 += (*string)[i];
 	}
 	if (sum1 > sum2) {
 		int offset = sum1 - sum2;
-		for (i = countOfNumbers - 1; i >= countOfNumbers / 2; i--) {
+		for (i = (*countOfNumbers) - 1; i >= (*countOfNumbers) / 2; i--) {
 			if (offset + (*string)[i] <= 9) {
 				(*string)[i] += offset;
 				break;
@@ -36,22 +39,16 @@ void transform(int sum1, int sum2, int reSumSum1AndSum2, char** string, int coun
 		}
 	} else if (sum1 < sum2) {
 		int sum = 0;
-		for (i = countOfNumbers / 2; i < countOfNumbers; i++) {
+		for (i = (*countOfNumbers) / 2; i < (*countOfNumbers); i++) {
 			sum += (*string)[i];
 			if (sum >= sum1) {
 				//(*string)[i-1]++;
-				int reSum = addOneToDigit(string, i-1);
-				if (i != countOfNumbers / 2) {
-					sum2++;	 	
-				} else {
-					sum1++;
-				}
+				addOneToDigit(string, i-1, countOfNumbers);
 				int j;
-				for (j = i; j < countOfNumbers; j++) {
-					sum2 -= (*string)[j];
+				for (j = i; j < (*countOfNumbers); j++) {
 					(*string)[j] = 0;
 				}
-				transform(sum1, sum2, reSum, string, countOfNumbers);
+				transform(string, countOfNumbers);
 				break;
 			} 
 		}
@@ -59,19 +56,20 @@ void transform(int sum1, int sum2, int reSumSum1AndSum2, char** string, int coun
 }
 
 int main(void) {
-	char* string = malloc(81); 
+	char string[100];
 	scanf("%s", string);
 	int i;
 	for (i = 0; string[i] != '\0'; i++) {
 		string[i] -= '0';
 	}
 	int countOfNumbers = i;
-	// Exclusive sutiation when were entered numbers from 1 to 9
+	// Exclusive situation when were entered numbers from 1 to 9
 	if (countOfNumbers == 1) {
 		printf("11");
 		return 0;
 	}
-	transform(0, 0, 1, &string, countOfNumbers);
+	char* temp = ((char *)string);
+	transform(&temp, &countOfNumbers);
 	for (i = 0; i < countOfNumbers; i++) {
 		printf("%d", string[i]);
 	}
