b4de4249fd9611f5e178fa0ec32fb82419bf363b
Nikita Bobko <nikitabobko@gmail.com>
2016-11-03 01:40:00 +0300

    Second attemption to solve 04/06: too slow ¯\_(ツ)_/¯

--- 04/06.c
+++ 04/06.c
@@ -1,4 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct space
+{
+	int numberOfSpaces;
+	int index;
+};
+
+typedef struct space _space;
+
+/**
+ * Return 0 if equals
+ * 1 if s2 bigger than s1
+ * -1 if s1 bigger than s2
+ * compareMode - Mode of comparing 1 - compare by value and index, 2 - compare only by index
+ */
+int equals(_space s1, _space s2, char compareMode) {
+	if (s1.numberOfSpaces == s2.numberOfSpaces && s1.index == s2.index) return 0;
+	if (compareMode == 1) {
+		return (s1.numberOfSpaces < s2.numberOfSpaces || 
+			(s1.numberOfSpaces == s2.numberOfSpaces && s1.index < s2.index)) ? 1 : -1;
+	} else {	// compareMode == 2
+		return s1.index < s2.index ? 1 : -1;
+	}
+}
+
+void swap(_space* s1, _space* s2) {
+	_space temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+void sort(_space* x, int first, int last, char compareMode) {
+	if (first < last) {
+		int pivot, j, i;
+		pivot = first;
+		i = first;
+		j = last;
+		while (i < j) {
+			while (equals(x[i], x[pivot], compareMode) == 1 && i < last) {
+				i++;
+			}
+			while (equals(x[j], x[pivot], compareMode) == -1) {
+				j--;
+			}
+			if (i < j) {
+				swap(&x[i], &x[j]);
+			}
+		}
+		_space* temp = &x[pivot];
+		x[pivot] = x[j];
+		x[j] = *temp;
+		sort(x, first, j - 1, compareMode);
+		sort(x, j + 1, last, compareMode);
+	}
+}
 
 int main(void) {
 	int k;
@@ -7,32 +63,33 @@ int main(void) {
 	getchar();
 	fgets(str, k + 2, stdin);
 	int i, numberOfSpaces = 0;
-	int spacesArr[k];
+	_space* spacesArr = malloc(k * sizeof(_space));
 	for (i = 0; str[i] != '\n'; i++) {
-		spacesArr[numberOfSpaces] = 0;
 		if (str[i] == ' ') {
+			spacesArr[numberOfSpaces].numberOfSpaces = 0;
+			spacesArr[numberOfSpaces].index = numberOfSpaces;
 			while (str[i] == ' ') {
-				spacesArr[numberOfSpaces]++;
+				spacesArr[numberOfSpaces].numberOfSpaces++;
 				i++;
 			}
 			numberOfSpaces++;
 			i--;
 		}
 	}
+	sort(spacesArr, 0, numberOfSpaces-1, 1);
 	// i - lenght
 	for (i = k - i; i > 0; i--) {
-		int minIndex = 0, j;
-		for (j = 1; j < numberOfSpaces; j++) {
-			if (spacesArr[j] < spacesArr[minIndex] || 
-					(spacesArr[j] == spacesArr[minIndex] && j < minIndex)) {
-				minIndex = j;
-			}
+		spacesArr[0].numberOfSpaces++;
+		int j;
+		for (j = 1; equals(spacesArr[j-1], spacesArr[j], 1) == -1 && j < numberOfSpaces; j++) {
+			swap(&spacesArr[j-1], &spacesArr[j]);
 		}
-		spacesArr[minIndex]++;
 	}
+	// Sort only by index
+	sort(spacesArr, 0, numberOfSpaces-1, 2);
 	// Now use numberOfSpaces as index
 	numberOfSpaces = 0;
-	for (i = 0; str[i] != '\0'; i++) {
+	for (i = 0; str[i] != '\n'; i++) {
 		if (str[i] != ' ') printf("%c", str[i]);
 		else {
 			while (str[i] == ' ') {
@@ -40,7 +97,7 @@ int main(void) {
 			}
 			i--;
 			int j;
-			for (j = 0; j < spacesArr[numberOfSpaces]; j++) {
+			for (j = 0; j < spacesArr[numberOfSpaces].numberOfSpaces; j++) {
 				printf(" ");
 			}
 			numberOfSpaces++;
