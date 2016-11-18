9b3bb0664aedb31b6f6fd5c1131f382817ac6b49
Nikita Bobko <nikitabobko@gmail.com>
2016-11-19 00:42:12 +0300

    Code refactor

--- 01/01.c
+++ 01/01.c
@@ -4,15 +4,15 @@ int main(void) {
     int n;
     scanf("%d", &n);
     if (n == 1) {
-	printf("0");
-	return 0;
-    }
-    int i, min = -1, max = -1;
-    for (i = 0; i < n; i++) {
-	int cur;
-	scanf("%d", &cur);
-	if (min == -1 || cur < min) min = cur;
-	if (max == -1 || cur > max) max = cur;
-    }
-    printf("%d", max-min);
+       printf("0");
+       return 0;
+   }
+   int i, min = -1, max = -1;
+   for (i = 0; i < n; i++) {
+       int cur;
+       scanf("%d", &cur);
+       if (min == -1 || cur < min) min = cur;
+       if (max == -1 || cur > max) max = cur;
+   }
+   printf("%d", max-min);
 }
