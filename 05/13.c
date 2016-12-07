e781fb3b7526e4ffe393a4997df4eff2c6786e24
Nikita Bobko <nikitabobko+git@gmail.com>
2016-12-07 21:56:24 +0300

    Solve 05-13

--- 05/13.c
+++ 05/13.c
@@ -2,70 +2,67 @@
 #include <stdlib.h>
 #include <math.h>
 
-struct list
-{
+typedef struct structCell Cell;
+
+struct structCell {
 	int y;
 	int x;
-	struct list *next;
-	struct list *previous;
+	int indexInBinHeap;
+	int weight;
+	int height;
+	char isPinned;
 };
 
-typedef struct list LinkedList;
-
-int **allocateMatrix(int n, int m, const char isMalloc) {
-	int **matrix = malloc(n * sizeof(int*));
+Cell ***allocateMatrix(int n, int m) {
+	Cell ***matrix = malloc(n * sizeof(Cell **));
 	for (int i = 0; i < n; i++) {
-		if (isMalloc) matrix[i] = malloc(m * sizeof(int));
-		else matrix[i] = calloc(m, sizeof(int));
+		matrix[i] = malloc(m * sizeof(Cell *));
 	}
 	return matrix;
 }
 
-LinkedList *addToList(LinkedList *linkedList, int y, int x, int** const graph) {
-	LinkedList* curElement = linkedList;
-	for (;curElement != NULL && curElement->next != NULL 
-		&& graph[curElement->next->y][curElement->next->x] < graph[y][x]; 
-		curElement = curElement->next);
-	LinkedList* insert = malloc(sizeof(LinkedList));
-	insert->x = x;
-	insert->y = y;
-	if (curElement == NULL) {	
-		insert->previous = NULL;
-		insert->next = NULL;
-		return insert;
-	} else {
-		if (curElement->next == NULL) {
-			curElement->next = insert;
-			insert->previous = curElement;
-			insert->next = NULL;
+void swap(Cell **binHeap, int index1, int index2) {
+	Cell *temp = binHeap[index1];
+	binHeap[index1] = binHeap[index2];
+	binHeap[index2] = temp;
+	// Fix indexes
+	binHeap[index1]->indexInBinHeap = index1;
+	binHeap[index2]->indexInBinHeap = index2;
+}
+
+void heapify(Cell **binHeap, int index) {
+	while (index != 0 
+			&& binHeap[(index - 1) / 2]->weight > binHeap[index]->weight) {
+		swap(binHeap, (index - 1) / 2, index);
+		index = (index - 1) / 2;
+	}
+}
+
+Cell *popHead(Cell **binHeap, int *howManyCellsFilled) {
+	(*howManyCellsFilled)--;
+	Cell *head = binHeap[0];
+	if (*howManyCellsFilled == 0) return head;
+	binHeap[0] = binHeap[*howManyCellsFilled];
+	binHeap[0]->indexInBinHeap = 0;
+	int index = 0;
+	while (1) {
+		int child1 = 2 * index + 1, child2 = 2 * index + 2, minChildIndex;
+		if (child1 >= *howManyCellsFilled && child2 >= *howManyCellsFilled) {
+			break;
+		} else if (child1 >= *howManyCellsFilled || child2 >= *howManyCellsFilled) {
+			minChildIndex = (child1 >= *howManyCellsFilled ? child2 : child1);
 		} else {
-			insert->previous = curElement;
-			insert->next = curElement->next;
-			insert->next->previous = insert;
-			curElement->next = insert;
+			minChildIndex = binHeap[child1]->weight < binHeap[child2]->weight ?
+				child1 : child2;
 		}
-		return linkedList;
+		if (binHeap[index]->weight <= binHeap[minChildIndex]->weight) break;
+		swap(binHeap, index, minChildIndex);
+		index = minChildIndex;
 	}
+	return head;
 }
 
-LinkedList *removeFromList(LinkedList *linkedList, LinkedList *objToRemove) {
-	LinkedList *newHead = linkedList;
-	if (objToRemove->previous != NULL) objToRemove->previous->next = objToRemove->next;
-	else newHead = objToRemove->next;
-	if (objToRemove->next != NULL) objToRemove->next->previous = objToRemove->previous;
-	free(objToRemove);
-	return newHead;
-}
-
-char **allocateCharMatrix(int n, int m) {
-	char **matrix = malloc(n * sizeof(char*));
-	for (int i = 0; i < n; i++) {
-		matrix[i] = calloc(m, 1);
-	}
-	return matrix;
-}
-
-void deallocateMatrix(void **matrix, int n) {
+void deallocateMatrix(Cell ***matrix, int n) {
 	for (int i = 0; i < n; i++) {
 		free(matrix[i]);
 	}
@@ -75,47 +72,63 @@ void deallocateMatrix(void **matrix, int n) {
 int main(void) {
 	int n, m, startY, startX, endX, endY;
 	scanf("%d%d%d%d%d%d", &n, &m, &startY, &startX, &endY, &endX);
-	int** map = allocateMatrix(n, m, 1);
+	Cell ***matrix = allocateMatrix(n, m);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			scanf("%d", &(map[i][j]));
+			matrix[i][j] = malloc(sizeof(Cell));
+			matrix[i][j]->indexInBinHeap = -1;
+			matrix[i][j]->weight = -1;
+			matrix[i][j]->isPinned = 0;
+			matrix[i][j]->y = i;
+			matrix[i][j]->x = j;
+			scanf("%d", &(matrix[i][j]->height));
 		}
 	}
-	int **graph = allocateMatrix(n, m, 0);
-	char **isPinned = allocateCharMatrix(n, m);
 	const char xOffset[] = {-1, 0, 1, 0};
 	const char yOffset[] = {0, -1, 0, 1};
-	isPinned[startY][startX] = 1;
-	LinkedList *linkedList = NULL;
+	matrix[startY][startX]->isPinned = 1;
+	matrix[startY][startX]->weight = 0;
+	int howManyCellsFilled = 0;
+	Cell **binHeap = malloc(n * m * sizeof(Cell *));
 
 	int y = startY, x = startX;
-	while (!isPinned[endY][endX]) {
+	while (!(matrix[endY][endX]->isPinned)) {
 		for (int i = 0; i < 4; i++) {
 			int newY = y + yOffset[i], newX = x + xOffset[i];
-			if (newY >= 0 && newX >= 0 && newX < m && newY < n
-				&& (abs(endY - newY) < abs(endY - y) || abs(endX - newX) < abs(endX - x))
-				&& !isPinned[newY][newX] && (graph[newY][newX] == 0
-				|| graph[newY][newX] > graph[y][x] + abs(map[newY][newX] - map[y][x]))) {
-				graph[newY][newX] = graph[y][x] + abs(map[newY][newX] - map[y][x]);
-				linkedList = addToList(linkedList, newY, newX, graph);
+			if (newY >= 0 && newX >= 0 && newX < m && newY < n) {
+				int oldWeight = matrix[newY][newX]->weight,
+					newWeight = matrix[y][x]->weight + 
+					abs(matrix[newY][newX]->height - matrix[y][x]->height);
+				if (!(matrix[newY][newX]->isPinned) && (oldWeight == -1
+					|| oldWeight > newWeight)) {
+					matrix[newY][newX]->weight = newWeight;
+					int index;
+					if (matrix[newY][newX]->indexInBinHeap != -1) {
+						index = matrix[newY][newX]->indexInBinHeap;
+					} else {
+						index = howManyCellsFilled;
+						matrix[newY][newX]->indexInBinHeap = index;
+						binHeap[index] = matrix[newY][newX];
+						howManyCellsFilled++;
+					}
+					heapify(binHeap, index);
+				}
 			}
 		}
-		// LinkedList *minElement = NULL, *curElement;
-		// for (curElement = linkedList; curElement != NULL; curElement = curElement->next) {
-		// 	if (minElement == NULL || 
-		// 		graph[curElement->y][curElement->x] < graph[minElement->y][minElement->x]){
-		// 		minElement = curElement;
-		// 	}
-		// }
-		isPinned[linkedList->y][linkedList->x] = 1;
-		y = linkedList->y;
-		x = linkedList->x;
-		linkedList = removeFromList(linkedList, linkedList);
+
+		Cell *min = popHead(binHeap, &howManyCellsFilled);
+
+		matrix[min->y][min->x]->isPinned = 1;
+		y = min->y;
+		x = min->x;
 	}
 
-	printf("%d\n", graph[endY][endX]);
-	deallocateMatrix((void **) isPinned, n);
-	deallocateMatrix((void **) graph, n);
-	deallocateMatrix((void **) map, n);
+	printf("%d\n", matrix[endY][endX]->weight);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			free(matrix[i][j]);
+		}
+	}
+	deallocateMatrix(matrix, n);
 	return 0;
 }
