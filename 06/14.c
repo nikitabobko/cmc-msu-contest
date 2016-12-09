79be20d589d04afdf6d4b21a92238ff42ef9b92a
Nikita Bobko <nikitabobko+git@gmail.com>
2016-12-09 15:32:10 +0300

    In progress

--- 06/14.c
+++ 06/14.c
@@ -4,27 +4,29 @@ int main(void) {
 	FILE *in = fopen("matrix.in", "r");
 	FILE *out = fopen("trace.out", "w");
 
-	short n = 0;
+	unsigned short n = 0;
 	for (int i = 0; i < 2; i++) {
-		char s;
+		unsigned char s;
 		fread(&s, sizeof(char), 1, in);
 		n = (n << 8) | s;
 	}
-	long long summ = 0;
+	unsigned long long summ = 0;
 	for (int i = 0; i < n; i++) {
-		int num = 0;
+		unsigned int num = 0;
 		for (int i = 0; i < 4; i++) {
-			char s;
+			unsigned char s;
 			fread(&s, sizeof(char), 1, in);
 			num = (num << 8) | s;
 		}
+		// 2^31 = Ox80000000
+		if (num >= Ox80000000) 0xFFFFFFFFFFFFFFFF
 		summ += num;
 		for (int i = 0; i < n; i++) {
 			fread(&num, sizeof(char), 4, in);
 		}
 	}
 	for (int i = 0; i < 8; i++) {
-		long long s = summ >> ((7 - i) * 8);
+		unsigned long long s = summ >> ((7 - i) * 8);
 		fwrite(&s, sizeof(char), 1, out);
 	}
 
