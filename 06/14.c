5099c81221eae630579890529d4f207dd0bc2b15
Nikita Bobko <nikitabobko@gmail.com>
2016-12-10 00:36:17 +0300

    Solve 06-14

--- 06/14.c
+++ 06/14.c
@@ -4,30 +4,32 @@ int main(void) {
 	FILE *in = fopen("matrix.in", "r");
 	FILE *out = fopen("trace.out", "w");
 
-	unsigned short n = 0;
+	int n = 0;
 	for (int i = 0; i < 2; i++) {
-		unsigned char s;
+		int s = 0;
 		fread(&s, sizeof(char), 1, in);
 		n = (n << 8) | s;
 	}
-	unsigned long long summ = 0;
+	long long trace = 0;
 	for (int i = 0; i < n; i++) {
-		unsigned int num = 0;
+		int num = 0;
 		for (int i = 0; i < 4; i++) {
-			unsigned char s;
+			int s = 0;
 			fread(&s, sizeof(char), 1, in);
 			num = (num << 8) | s;
 		}
-		// 2^31 = Ox80000000
-		if (num >= Ox80000000) 0xFFFFFFFFFFFFFFFF
-		summ += num;
+		trace += num;
+		// Skip for n numbers
 		for (int i = 0; i < n; i++) {
 			fread(&num, sizeof(char), 4, in);
 		}
 	}
+	long long mask = 0xFF;
+	mask <<= (7 * 8);
 	for (int i = 0; i < 8; i++) {
-		unsigned long long s = summ >> ((7 - i) * 8);
+		long long s = (trace & mask) >> ((7 - i) * 8);
 		fwrite(&s, sizeof(char), 1, out);
+		mask >>= 8;
 	}
 
 	fclose(out);
