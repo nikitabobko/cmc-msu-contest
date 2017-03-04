50e099e90547402b983871a47f9c6b860bc254f9
Nikita Bobko <nikitabobko@gmail.com>
2017-03-05 00:06:17 +0300

    In progress

--- asm/03/14.asm
+++ asm/03/14.asm
@@ -2,20 +2,47 @@
 
 section .text
 global CMAIN
+global combination
 CMAIN:
     mov ebp, esp
     
+    GET_UDEC 4, ebx
+    GET_UDEC 4, esi
+
+    mov eax, ebx    
+    mov ecx, 0x20
+.loop:
+    shr eax, 1
+    loopnz .loop
+    
+    ; edi = number of bits in number
+    mov edi, 0x20
+    sub edi, ecx 
+    
+    ; eax = combination(n-1, k+1) = combination(n-1, k) + 
+    ; combination(n-2,k) + ... + combination(n-(n-k), k)
+    inc  esi
+    push esi
+    dec  esi
+    dec  edi
+    push edi
+    inc  edi
+    call combination
+    
+    mov ecx, eax
+    
+    
+    mov esp, ebp
+    xor eax, eax
     ret
     
-    
-global combination
 ; int combination(int n, int k) = n!/(k!(n-k)!)
 combination:
     push ebp
     mov ebp, esp
     mov edx, [ebp+8]
     
-    cmp edx, [esp+12]
+    cmp edx, [ebp+12]
     jb .exit
 
     mov edx, [ebp+8]
@@ -23,15 +50,16 @@ combination:
     mov eax, 0x1
     mov ecx, edx
     sub ecx, 2
-    js .endloop1
+    js .endloop0
 .loop0:
     mul edx
     dec edx
-    loop .loop1
+    loop .loop0
 .endloop0:
     ; [ebp+4] = (n-k)!
     push eax
     
+    ; eax = n!/k!
     mov edx, [ebp+8]
     mov ecx, edx
     sub ecx, [ebp+12]
@@ -43,13 +71,14 @@ combination:
     loop .loop
 .endloop:
     
-    ; ecx = (n-k)!
-    pop ecx
+    pop ecx ; ecx = (n-k)!
     xor edx, edx
     div ecx
 
+    pop ebp
     ret
     
 .exit:
+    pop ebp
     xor eax, eax
     ret
