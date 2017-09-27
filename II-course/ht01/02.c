ac4dfab43e766d6ca2c16c90b9fccb4e81d6ac83
Nikita Bobko <nikitabobko@gmail.com>
2017-09-27 23:19:06 +0300

    Pending review

--- II-course/ht01/02.c
+++ II-course/ht01/02.c
@@ -8,6 +8,7 @@ int main(int argc, char const *argv[])
 {
     for (int i = 1; i < argc; i++) {
         char *endptr = NULL;
+        errno = 0;
         unsigned long int val = strtoul(argv[i], &endptr, 10);
         if ((endptr != NULL && *endptr != '\0') || errno == ERANGE || val > UINT32_MAX) {
             printf("-1\n");
