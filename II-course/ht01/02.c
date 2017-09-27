b438aa2739362a7398d79e40462fd05b2992ed17
Nikita Bobko <nikitabobko+git@gmail.com>
2017-09-27 22:25:59 +0300

    Presentation error

--- II-course/ht01/02.c
+++ II-course/ht01/02.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <math.h>
 #include <errno.h>
-#include <limits.h>
+#include <stdint.h>
 
 int main(int argc, char const *argv[])
 {
     for (int i = 1; i < argc; i++) {
         char *endptr = NULL;
         unsigned long int val = strtoul(argv[i], &endptr, 10);
-        if ((endptr != NULL && *endptr != '\0') || (val == ULONG_MAX && errno == ERANGE)) {
+        if ((endptr != NULL && *endptr != '\0') || errno == ERANGE || val > UINT32_MAX) {
             printf("-1\n");
             continue;
         }
