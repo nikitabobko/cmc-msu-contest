40ac8a801ee13666bb03f61a850956fd3c2a4850
Nikita Bobko <nikitabobko@gmail.com>
2017-09-29 23:15:58 +0300

    Pending review

--- II-course/04/01.c
+++ II-course/04/01.c
@@ -5,7 +5,10 @@ int main(void) {
     if (scanf("%u %u", &n, &w) != 2) {
         return 1;
     }
-    unsigned int number = 0, mask = (0x1 << n) - 1, overflowed = mask + 1, max = mask >> 1;
+    unsigned int number = 0;
+    unsigned int mask = (0x1 << n) - 1;
+    unsigned int overflowed = mask + 1;
+    unsigned int max = mask >> 1;
     do {
         int n = number > max ? (-1)*(overflowed - number) : number;
         printf("|%*u|%*x|%*d|\n", w, number, w, number, w, n);
