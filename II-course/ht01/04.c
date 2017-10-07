3cc923fa2a9870cee58b4e96f22ab100e2959b6f
Nikita Bobko <nikitabobko+git@gmail.com>
2017-10-07 22:52:36 +0300

    Pending review

--- II-course/ht01/04.c
+++ II-course/ht01/04.c
@@ -7,54 +7,57 @@ bsearch2(
         int (*compar)(const void *p1, const void *p2, void *user),
         void *user,
         ssize_t *p_low,
-        ssize_t *p_high) 
+        ssize_t *p_high)
 {
     *p_high = 0;
     *p_low = 0;
-    
+
     int ret = 0;
-    char search_p_high_mode = 0;
 
     const char *base_backup = NULL;
-    ssize_t nmemb_backup;
-    ssize_t p_high_backup;
+    ssize_t nmemb_backup = 0;
+    ssize_t p_high_backup = 0;
 
-    while (nmemb > 0 || !search_p_high_mode) {
-        if (nmemb == 0 && !search_p_high_mode) {
-            if (base_backup == NULL) {
-                break;
-            }
-            base = base_backup;
-            nmemb = nmemb_backup;
-            *p_high = p_high_backup;
-            search_p_high_mode = 1;
-        }
+    while (nmemb > 0) {
         ssize_t middle_index = nmemb / 2;
         const char *middle = (const char *) base + middle_index * size;
         int compared = compar(key, middle, user);
-        if ((!search_p_high_mode && compared > 0) || (search_p_high_mode && compared == 0)) {
+        if (compared > 0) {
             base = middle + size;
             nmemb = nmemb - 1 - middle_index;
             *p_high += middle_index + 1;
-            if (!search_p_high_mode) {
-                *p_low += middle_index + 1;
-            }
-            continue;
-        }
-        if ((!search_p_high_mode && compared < 0) || search_p_high_mode) {
+            *p_low += middle_index + 1;
+        } else if (compared < 0) {
             nmemb = middle_index;
-            continue;
-        }
-        // go to search p_low
-        // but firstly backup current state to be able to find p_high
-        if (base_backup == NULL) {
-            base_backup = base;
-            nmemb_backup = nmemb;
-            p_high_backup = *p_high;
-        }
+        } else {
+            if (base_backup == NULL) {
+                base_backup = base;
+                nmemb_backup = nmemb;
+                p_high_backup = *p_high;
+            }
 
-        ret = 1;
-        nmemb = middle_index;
+            ret = 1;
+            nmemb = middle_index;
+        }
+    }
+    if (!ret) {
+        return ret;
+    }
+    // Search p_high
+    base = base_backup;
+    nmemb = nmemb_backup;
+    *p_high = p_high_backup;
+    while (nmemb > 0) {
+        ssize_t middle_index = nmemb / 2;
+        const char *middle = (const char *) base + middle_index * size;
+        int compared = compar(key, middle, user);
+        if (compared == 0) {
+            base = middle + size;
+            nmemb = nmemb - 1 - middle_index;
+            *p_high += middle_index + 1;
+        } else {
+            nmemb = middle_index;
+        }
     }
     return ret;
 }
