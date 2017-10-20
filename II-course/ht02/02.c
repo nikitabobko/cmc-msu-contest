577b937641e72710614b945403e77cf6f723f9fe
Nikita Bobko <nikitabobko@gmail.com>
2017-10-20 19:12:43 +0300

    N       Result  Time (sec)      Score 1       OK      0.383   0 (0) 2       OK      0.384   0 (0) 3       OK      0.390   0 (0) 4       OK      0.390   0 (0) 5       Time-limit exceeded     >2.000  0 (0) 6       Run-time error  0.391   0 (0) 7       Run-time error  0.391   0 (0) 8       Run-time error  0.391   0 (0) 9       Run-time error  1.896   0 (0) 10      Time-limit exceeded     >2.000  0 (0) 11      Run-time error  0.450   0 (0) 12      Run-time error  0.450   0 (0) 13      Run-time error  0.450   0 (0)

--- II-course/ht02/02.c
+++ II-course/ht02/02.c
@@ -7,41 +7,42 @@ enum
     MAX_SPACE_CODE_POINT = 0x20
 };
 
-int code_point_to_bytes(char *str) {
-    if (*str >> 7 == 0) {
+int code_point_to_bytes(unsigned char c) {
+    if (c >> 7 == 0) {
         return 1;
-    } else if (*str >> 5 == 0x6) {
+    } else if (c >> 5 == 0x6) {
         return 2;
-    } else if (*str >> 4 == 0xe) {
+    } else if (c >> 4 == 0xe) {
         return 3;
-    } else if (*str >> 3 == 0x1e) {
+    } else if (c >> 3 == 0x1e) {
         return 4;
     }
-    return -1;
+    exit(1);
 }
 
-int read_word(unsigned char *str, unsigned char **word_start, unsigned char **word_end) {
-    if (str == NULL || *str == '\0') {
+int length(unsigned char *str) {
+    if (str == NULL) {
         return 0;
     }
-    while (*str <= MAX_SPACE_CODE_POINT) {
-        str++;
-    }
-    *word_start = str;
     int len = 0;
-    while (*str > MAX_SPACE_CODE_POINT) {
-        str += code_point_to_bytes(str);
+    while (*str != '\0' && *str > MAX_SPACE_CODE_POINT) {
+        str += code_point_to_bytes(*str);
         len++;
     }
-    *word_end = str;
     return len;
 }
 
-unsigned char *read_input() {
+unsigned char *read_word(void) {
+    int c;
+    do {
+        c = fgetc(stdin);
+    } while (c != EOF && c <= MAX_SPACE_CODE_POINT);
+    if (c == EOF) {
+        return NULL;
+    }
     unsigned char *str = NULL;
     int size = 0, pos = 0;
-    int c;
-    while ((c = fgetc(stdin)) != EOF) {
+    do {
         if (str == NULL) {
             size = DEFAULT_CAPACITY;
             str = malloc(size * sizeof(*str));
@@ -51,40 +52,35 @@ unsigned char *read_input() {
         }
         if (pos + 1 >= size) {
             size *= 2;
-            str = realloc(str, size * sizeof(*str));
-            if (str == NULL) {
+            char *ptr = realloc(str, size * sizeof(*str));
+            if (ptr == NULL) {
+                free(str);
                 return NULL;
             }
+            str = ptr;
         }
         str[pos++] = (c == '\0' ? MAX_SPACE_CODE_POINT : c);
-    }
-    if (str != NULL) {
-        str[pos] = '\0';
-    }
+    } while ((c = fgetc(stdin)) != EOF && c > MAX_SPACE_CODE_POINT);
+    str[pos] = '\0';
     return str;
 }
 
-int main(int argc, unsigned char const *argv[]) {
-    unsigned char *str = read_input();
-
-    unsigned char *word_start, *word_end = str;
-    unsigned char *max_word_start = NULL;
-    unsigned char *max_word_end = NULL;
+int main(void) {
     int max_len = 0, len;
-    while ((len = read_word(word_end, &word_start, &word_end))) {
-        if (len > max_len) {
+    char *max_len_str = NULL, *str;
+    while ((str = read_word())) {
+        if ((len = length(str)) > max_len) {
             max_len = len;
-            max_word_start = word_start;
-            max_word_end = word_end;
+            free(max_len_str);
+            max_len_str = str;
         }
     }
 
     printf("%d\n", max_len);
     if (max_len > 0) {
-        *max_word_end = '\0';
-        printf("%s\n", max_word_start);
+        printf("%s\n", max_len_str);
     }
 
-    free(str);
+    free(max_len_str);
     return 0;
 }
