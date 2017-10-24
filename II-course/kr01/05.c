ac76adcdab00146d95c9b1da0795429b09aa58a5
Nikita Bobko <nikitabobko@gmail.com>
2017-10-25 00:40:05 +0300

    N       Result  Time (sec)      Score 1       OK      0.001   0 (0) 2       Wrong answer    0.001   0 (0) 3       OK      0.001   0 (0) 4       OK      0.001   0 (0) 5       OK      0.001   0 (0) 6       Run-time error  0.001   0 (0) 7       OK      0.001   0 (0) 8       Wrong answer    0.001   0 (0) 9       OK      0.001   0 (0)

--- II-course/kr01/05.c
+++ II-course/kr01/05.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <dirent.h>
-
 
 int main(int argc, const char *argv[]) {
     if (argc != 4) {
-        return 1;
+        return 0;
     }
-    unsigned const char *dir_name = argv[1];
+    const char *dir_name = argv[1];
     int64_t limit = strtol(argv[2], NULL, 10);
-    unsigned const char *suffix = argv[3];
+    const char *suffix = argv[3];
     int suffix_len = strlen(suffix);
 
     int64_t sum = 0;
     DIR *dir = opendir(dir_name);
     if (dir == NULL) {
-        return 1;
+        return 0;
     }
     struct dirent *dd;
-    char file_name[PATH_MAX] = {};
-    off_t max_file_name_size = 0;
+    char *file_name = NULL;
+    off_t max_size = 0;
     while ((dd = readdir(dir))) {
-        char file[PATH_MAX];
+        char file[PATH_MAX + 1];
         snprintf(file, sizeof(file), "%s/%s", dir_name, dd->d_name);
         struct stat info;
         if (lstat(file, &info)) {
-            return 1;
+            continue;
         }
         if (S_ISREG(info.st_mode)) {
             sum += info.st_size;
-            if (info.st_size > max_file_name_size && 
-                    strcmp(dd->d_name + strlen(dd->d_name) - suffix_len, suffix) == 0 && 
-                    (*file_name == '\0' || strcmp(file_name, dd->d_name) > 0)) {
-                strcpy(file_name, dd->d_name);
-                max_file_name_size = info.st_size;
+            if (info.st_size > max_size &&
+                    strcmp(dd->d_name + strlen(dd->d_name) - suffix_len, suffix) == 0 &&
+                    (file_name == NULL || strcmp(file_name, dd->d_name) > 0)) {
+                free(file_name);
+                file_name = strdup(dd->d_name);
+                max_size = info.st_size;
             }
         }
     }
 
-    if (sum > limit && *file_name != '\0') {
+    if (sum > limit && file_name != NULL) {
         printf("%s\n", file_name);
     }
+    free(file_name);
+    closedir(dir);
     return 0;
 }
