1808baea5619f7590a1635b7f8007d2c8d32765d
Nikita Bobko <nikitabobko@gmail.com>
2017-10-27 02:14:02 +0300

    All runtime errors

--- II-course/07/04.c
+++ II-course/07/04.c
@@ -45,14 +45,3 @@ RandomGenerator *random_create(int seed) {
     ptr->previous = seed;
     return ptr;
 }
-
-int main(int argc, char const *argv[])
-{
-    RandomGenerator *rr = random_create(1234);
-    for (int j = 0; j < 100; ++j) {
-        printf("%d\n", rr->ops->next(rr));
-    }
-    rr->ops->destroy(rr);
-
-    return 0;
-}
