bf4ed2e31efe14b18be42105aca45a382d2eaa6c
Nikita Bobko <nikitabobko+git@gmail.com>
2017-10-28 15:08:50 +0300

    WTF?! More Run-time errors
    N       Result  Time (sec)      Score
    1       OK      0.001   0 (0)
    2       Run-time error  0.001   0 (0)
    3       Run-time error  0.001   0 (0)
    4       Run-time error  0.001   0 (0)
    5       Run-time error  0.001   0 (0)
    6       Run-time error  0.001   0 (0)
    7       Run-time error  0.001   0 (0)
    8       Run-time error  0.001   0 (0)
    9       Run-time error  0.001   0 (0)

--- II-course/kr01/05.c
+++ II-course/kr01/05.c
@@ -5,34 +5,43 @@
 #include <string.h>
 #include <dirent.h>
 
-int main(int argc, const char *argv[]) {
-    if (argc != 4) {
+int check_suffix(const char *str, const char *suffix, size_t suffix_len) {
+    size_t str_len = strlen(str);
+    if (suffix_len > str_len) {
         return 0;
     }
-    const char *dir_name = argv[1];
+    return strcmp(str + str_len - suffix_len, suffix) == 0;
+}
+
+int main(int argc, const char *argv[]) {
+    if (argc != 4) {
+        return 1;
+    }
+    const char *dir_path = argv[1];
     int64_t limit = strtol(argv[2], NULL, 10);
     const char *suffix = argv[3];
-    int suffix_len = strlen(suffix);
+    size_t suffix_len = strlen(suffix);
 
     int64_t sum = 0;
-    DIR *dir = opendir(dir_name);
+    DIR *dir = opendir(dir_path);
     if (dir == NULL) {
-        return 0;
+        return 1;
     }
     struct dirent *dd;
     char *file_name = NULL;
     off_t max_size = 0;
     while ((dd = readdir(dir))) {
         char file[PATH_MAX + 1];
-        snprintf(file, sizeof(file), "%s/%s", dir_name, dd->d_name);
+        snprintf(file, sizeof(file), "%s/%s", dir_path, dd->d_name);
         struct stat info;
         if (lstat(file, &info)) {
-            continue;
+            free(file_name);
+            return 1;
         }
         if (S_ISREG(info.st_mode)) {
             sum += info.st_size;
             if (info.st_size > max_size &&
-                    strcmp(dd->d_name + strlen(dd->d_name) - suffix_len, suffix) == 0 &&
+                    check_suffix(dd->d_name, suffix, suffix_len) &&
                     (file_name == NULL || strcmp(file_name, dd->d_name) > 0)) {
                 free(file_name);
                 file_name = strdup(dd->d_name);
