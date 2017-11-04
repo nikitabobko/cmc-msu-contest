81ecc6126775dd90cb9cdac01042d9a49822f8d7
Nikita Bobko <nikitabobko+git@gmail.com>
2017-11-04 21:56:08 +0300

    Get rid of "magical numbers". Pending review

--- II-course/08/03.c
+++ II-course/08/03.c
@@ -5,13 +5,14 @@
 
 enum 
 {
-    BUF_SIZE = 8
+    BUF_SIZE = 8,
+    CHILDREN_NUM = 3
 };
 
 int main(void) {
     char buf[BUF_SIZE];
     setbuffer(stdin, buf, sizeof(buf));
-    for (int i = 1; i <= 3; i++) {
+    for (int i = 1; i <= CHILDREN_NUM; i++) {
         pid_t pid = fork();
         if (pid == 0) {
             int num;
@@ -23,7 +24,7 @@ int main(void) {
             return 1;
         }
     }
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < CHILDREN_NUM; i++) {
         wait(NULL);
     }
 }
