57481ff29d0868b2982d937e9e48a25522780f56
Nikita Bobko <nikitabobko+git@gmail.com>
2017-11-06 23:40:14 +0300

    Two wrong answers on 12th and 18th tests

--- II-course/ht03/01.c
+++ II-course/ht03/01.c
@@ -1,11 +1,11 @@
 #include <sys/mman.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 enum
@@ -13,18 +13,20 @@ enum
     NUMERAL_SYSTEM_BASE = 10
 };
 
-void get_offset(long start_line, long line, FILE *file, long *offset_to_line_in_bytes) {
-    long cur_line = start_line;
-    long byte_offset = 0;
+int get_offset(int start_line, int line, FILE *file) {
+    int cur_line = start_line;
+    int byte_offset = 0;
     int c;
     while (cur_line < line && (c = fgetc(file)) != EOF) {
-        cur_line += c == '\n';
+        if (c == '\n') {
+            cur_line++;
+        }
         byte_offset++;
     }
-    *offset_to_line_in_bytes = byte_offset;
+    return byte_offset;
 }
 
-void print_line(char *line) {
+void print_line(const char *line) {
     while (*line != '\n' && *line != '\0') {
         putchar(*line);
         line++;
@@ -32,73 +34,83 @@ void print_line(char *line) {
     putchar('\n');
 }
 
-void print_lines_inverse(char *point_to_line1, long offset_to_line2_from_line1) {
-    char *ptr = point_to_line1 + offset_to_line2_from_line1 - 2;
-    while (ptr + 1 > point_to_line1 && *ptr != '\n') {
+void print_lines_inverse(char *point_to_line1, int offset_to_line2_from_line1) {
+    char *ptr = point_to_line1 + offset_to_line2_from_line1 - 1;
+    do {
         ptr--;
-    }
+    } while (ptr + 1 > point_to_line1 && *ptr != '\n');
     ptr++;
     while (ptr >= point_to_line1) {
         print_line(ptr);
-        ptr -= 2;
-        while (ptr + 1 > point_to_line1 && *ptr != '\n') {
+        ptr--;
+        do {
             ptr--;
-        }
+        } while (ptr + 1 > point_to_line1 && *ptr != '\n');
         ptr++;
     }
 }
 
-void my_exit(FILE *file_to_close, int ret_code) {
-    fclose(file_to_close);
+void my_exit(FILE *file_to_close, const char *msg, int ret_code) {
+    if (msg) {
+        fprintf(stderr, "%s\n", msg);
+    }
+    if (file_to_close) {
+        fclose(file_to_close);
+    }
     exit(ret_code);
 }
 
+int is_regular_file(const char *path) {
+    struct stat buf;
+    if (lstat(path, &buf) < 0) {
+        my_exit(NULL, "Error while determening whether file is regular", 1);
+    }
+    return S_ISREG(buf.st_mode);
+}
+
 int main(int argc, char const **argv) {
-    errno = 0;
     if (argc != 4) {
-        fprintf(stderr, "There should exact 3 arguments\n");
-        return 1;
+        my_exit(NULL, "There should exact 3 arguments", 1);
     }
     const char *path = argv[1];
+    if (!is_regular_file(path)) {
+        my_exit(NULL, "File is not regular", 1);
+    }
     FILE *file = fopen(path, "r");
-    if (file == NULL || errno) {
-        fprintf(stderr, "%s\n", strerror(errno));
-        return 1;
+    if (file == NULL) {
+        my_exit(NULL, "Error occurred while opening the file", 1);
     }
 
     char *endptr1, *endptr2;
     long line1 = strtol(argv[2], &endptr1, NUMERAL_SYSTEM_BASE);
     long line2 = strtol(argv[3], &endptr2, NUMERAL_SYSTEM_BASE);
     if (*endptr1 != '\0' || *endptr2 != '\0') {
-        fprintf(stderr, "%s\n", errno == 0 ? "Error while parsing arguments\n" : strerror(errno));
-        my_exit(file, 1);
+        my_exit(file, "Error while parsing arguments", 1);
     }
     if (line1 <= 0 || line2 <= 0) {
-        fprintf(stderr, "Line number cannot be non postitive\n");
-        my_exit(file, 1);
+        my_exit(file, "Line number cannot be non postitive", 1);
     }
 
     if (line1 >= line2) {
-        my_exit(file, 0);
+        my_exit(file, NULL, 0);
     }
 
-    long offset_to_line1, offset_to_line2_from_line1;
-    get_offset(1, line1, file, &offset_to_line1);
-    get_offset(line1, line2, file, &offset_to_line2_from_line1);
+    int offset_to_line1, offset_to_line2_from_line1;
+    offset_to_line1 = get_offset(1, line1, file);
+    offset_to_line2_from_line1 = get_offset(line1, line2, file);
     if (offset_to_line2_from_line1 == 0) {
-        my_exit(file, 0);
+        my_exit(file, NULL, 0);
     }
 
     long page_size = sysconf(_SC_PAGE_SIZE);
-    long offset_in_page_to_line1 = offset_to_line1 % page_size;
+    int offset_in_page_to_line1 = offset_to_line1 % page_size;
     char *ptr = mmap(NULL, offset_to_line2_from_line1, PROT_READ, MAP_PRIVATE,
-                     fileno(file), offset_to_line1 - offset_in_page_to_line1);
+                    fileno(file), offset_to_line1 - offset_in_page_to_line1);
     if (ptr == MAP_FAILED) {
-        fprintf(stderr, "%s\n", strerror(errno));
-        my_exit(file, 1);
+        my_exit(file, "Error occurred while mapping the file", 1);
     }
 
     print_lines_inverse(ptr + offset_in_page_to_line1, offset_to_line2_from_line1);
-    my_exit(file, 0);
+    my_exit(file, NULL, 0);
     return 0;
 }
