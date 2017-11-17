64cd4ef3b66f37b30b13244112f5e66a7098f95d
Nikita Bobko <nikitabobko@gmail.com>
2017-11-18 01:45:29 +0300

    Fix stupid "Use mmap for processing" reject

--- II-course/ht03/01.c
+++ II-course/ht03/01.c
@@ -14,12 +14,11 @@ enum
     NUMERAL_SYSTEM_BASE = 10
 };
 
-int get_offset(int start_line, int line, FILE *file) {
+int get_offset(int start_line, int line, char *ptr, int *cur_pos) {
     int cur_line = start_line;
     int byte_offset = 0;
-    int c;
-    while (cur_line < line && (c = fgetc(file)) != EOF) {
-        if (c == '\n') {
+    while (cur_line < line && ptr[*cur_pos] != '\0') {
+        if (ptr[(*cur_pos)++] == '\n') {
             cur_line++;
         }
         byte_offset++;
@@ -63,11 +62,12 @@ void my_exit(FILE *file_to_close, const char *msg, int ret_code) {
     exit(ret_code);
 }
 
-int is_regular_file(const char *path) {
+int is_regular_file(const char *path, int *size) {
     struct stat buf;
     if (lstat(path, &buf) < 0) {
         my_exit(NULL, "Error while determening whether file is regular", 1);
     }
+    *size = buf.st_size;
     return S_ISREG(buf.st_mode);
 }
 
@@ -76,7 +76,8 @@ int main(int argc, char const **argv) {
         my_exit(NULL, "There should exact 3 arguments", 1);
     }
     const char *path = argv[1];
-    if (!is_regular_file(path)) {
+    int size;
+    if (!is_regular_file(path, &size)) {
         my_exit(NULL, "File is not regular", 1);
     }
 
@@ -95,26 +96,26 @@ int main(int argc, char const **argv) {
     if (file == NULL) {
         my_exit(NULL, "Error occurred while opening the file", 1);
     }
+    if (size == 0) {
+        my_exit(file, NULL, 0);    
+    }
+    char *ptr = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fileno(file), 0);
+    if (ptr == MAP_FAILED) {
+        my_exit(file, "Error occurred while mapping the file", 1);
+    }
 
     if (line1 >= line2) {
         my_exit(file, NULL, 0);
     }
 
-    int offset_to_line1 = get_offset(1, line1, file);
-    int offset_to_line2_from_line1 = get_offset(line1, line2, file);
+    int cur_pos = 0;
+    int offset_to_line1 = get_offset(1, line1, ptr, &cur_pos);
+    int offset_to_line2_from_line1 = get_offset(line1, line2, ptr, &cur_pos);
     if (offset_to_line2_from_line1 == 0) {
         my_exit(file, NULL, 0);
     }
 
-    long page_size = sysconf(_SC_PAGE_SIZE);
-    int offset_in_page_to_line1 = offset_to_line1 % page_size;
-    char *ptr = mmap(NULL, offset_to_line2_from_line1, PROT_READ, MAP_PRIVATE,
-                    fileno(file), offset_to_line1 - offset_in_page_to_line1);
-    if (ptr == MAP_FAILED) {
-        my_exit(file, "Error occurred while mapping the file", 1);
-    }
-
-    print_lines_inverse(ptr + offset_in_page_to_line1, offset_to_line2_from_line1);
+    print_lines_inverse(ptr + offset_to_line1, offset_to_line2_from_line1);
     my_exit(file, NULL, 0);
     return 0;
 }
