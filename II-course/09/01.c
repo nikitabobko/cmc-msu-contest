ad2b0d8a3e2e4fcd18e76873f50ccd1634ec97eb
Nikita Bobko <nikitabobko@gmail.com>
2017-11-20 23:50:52 +0300

    Pending review

--- II-course/09/01.c
+++ II-course/09/01.c
@@ -3,13 +3,14 @@
 
 enum 
 {
+    TABLE_ITEMS_COUNT = 1024,
     TABLE_SIZE = 4096,
     BUF_SIZE = 32,
     RIGHT_SHIFT_TO_GET_ROOT_TABLE_OFFSET = 10 + 12,
 };
 
 int get_range(FILE *f, unsigned *a, unsigned *b) {
-    static char buf[BUF_SIZE];
+    char buf[BUF_SIZE];
     if (!fgets(buf, sizeof(buf), f)) {
         return 0;
     }
@@ -24,7 +25,7 @@ int get_range(FILE *f, unsigned *a, unsigned *b) {
 }
 
 int main(int argc, char const *argv[]) {
-    char root_table[TABLE_SIZE] = {};
+    char root_table[TABLE_ITEMS_COUNT] = {};
     unsigned a, b, count = 0;
     while (get_range(stdin, &a, &b)) {
         for (unsigned i = a; i < b; i += TABLE_SIZE) {
