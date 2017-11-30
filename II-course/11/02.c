820ccc487efc541345be5d44929940180f47b9a5
Nikita Bobko <nikitabobko@gmail.com>
2017-11-30 12:00:54 +0300

    Edited mz version adding volatile keyword

---
+++ II-course/11/02.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef enum Mode {
+    Mode1, Mode2
+} Mode;
+
+volatile static Mode mode = Mode1;
+
+void sighandler(int sig) {
+    if (sig == SIGUSR1) {
+        mode = Mode1;
+    } else {
+        mode = Mode2;
+    }
+    signal(SIGUSR1, &sighandler);
+    signal(SIGUSR2, &sighandler);
+}
+
+int main(int argc, char const *argv[]) {
+    printf("%d\n", getpid());
+    fflush(stdout);
+    signal(SIGUSR1, &sighandler);
+    signal(SIGUSR2, &sighandler);
+
+    int num;
+    while (scanf("%d", &num) == 1) {
+        if (mode == Mode1) {
+            printf("%d\n", -num);
+        } else if (mode == Mode2) {
+            printf("%d\n", num * num);
+        }
+        fflush(stdout);
+    }
+    return 0;
+}
