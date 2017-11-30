23b45eab02507550b0fcd6b5b9e35fc4a47603b4
Nikita Bobko <nikitabobko@gmail.com>
2017-11-30 19:19:17 +0300

    Remove endian check

--- II-course/ht03/02/02.c
+++ II-course/ht03/02/02.c
@@ -8,35 +8,14 @@
 #include <limits.h>
 #include <errno.h>
 
-enum 
+enum
 {
     NUMERICAL_SYSTEM_BASE = 10,
 };
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 
-// Convert from little endian format to machine endian format
-static inline double from_little_endian(unsigned char *mem) {
-    double num = 0;
-    unsigned long long *buf = (unsigned long long *) &num;
-    *buf = 0;
-    for (int i = sizeof(num) - 1; i >= 0; i--) {
-        (*buf) <<= CHAR_BIT;
-        (*buf) |= mem[i];
-    }
-    return num;
-}
-
-// Convert from machine endian format to little endian format
-static inline void to_little_endian(unsigned char *mem, double num) {
-    unsigned long long *buf = (unsigned long long *) &num;
-    for (int i = 0; i < sizeof(num); i++) {
-        mem[i] = *buf;
-        (*buf) >>= CHAR_BIT;
-    }
-}
-
-static inline double get_elem(unsigned char *mem, int i, int j, int dim) {
+static inline double get_elem(double *mem, int i, int j, int dim) {
     if (i == j) {
         return 0.;
     }
@@ -45,18 +24,18 @@ static inline double get_elem(unsigned char *mem, int i, int j, int dim) {
         i = j;
         j = t;
     }
-    return from_little_endian(mem + (i * dim + j) * sizeof(double));
+    return mem[i * dim + j];
 }
 
-void fill_with_identity_matrix(unsigned char *mem, int dim) {
+void fill_with_identity_matrix(double *mem, int dim) {
     for (int i = 0; i < dim - 1; i++) {
         for (int j = i + 1; j < dim; j++) {
-            to_little_endian(mem + (i * dim + j) * sizeof(double), INFINITY);
+            mem[i * dim + j] = INFINITY;
         }
     }
 }
 
-void multiply(unsigned char *res, unsigned char *mem1, unsigned char *mem2, 
+void multiply(double *res, double *mem1, double *mem2,
         int dim, int *changes_happen) {
     int changes_happen_local = 0;
     for (int i = 0; i < dim - 1; i++) {
@@ -69,11 +48,11 @@ void multiply(unsigned char *res, unsigned char *mem1, unsigned char *mem2,
                     mem1_cur = temp;
                 }
                 double d = temp + get_elem(mem2, k, j, dim);
-                
+
                 cur = (k == 0 ? d : MIN(cur, d));
 
             }
-            to_little_endian(res + (i * dim + j) * sizeof(double), cur);
+            res[i * dim + j] = cur;
             if (mem1_cur > cur || mem1_cur < cur) {
                 changes_happen_local = 1;
             }
@@ -84,16 +63,16 @@ void multiply(unsigned char *res, unsigned char *mem1, unsigned char *mem2,
     }
 }
 
-unsigned char *pow_matrix(unsigned char *in_mem, int dim, int power, int size) {
-    unsigned char *mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 
+double *pow_matrix(double *in_mem, int dim, int power, int size) {
+    double *mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS,
             -1, 0);
-    unsigned char *res = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 
+    double *res = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS,
             -1, 0);
     if (mem == MAP_FAILED || res == MAP_FAILED) {
         munmap(in_mem, size);
         if (mem != MAP_FAILED) {
             munmap(mem, size);
-        } 
+        }
         if (res != MAP_FAILED) {
             munmap(res, size);
         }
@@ -106,14 +85,14 @@ unsigned char *pow_matrix(unsigned char *in_mem, int dim, int power, int size) {
     while (power > 0 && (changes_happen2 || changes_happen1)) {
         if (power & 0x1 && changes_happen1) {
             multiply(mem, res, in_mem, dim, &changes_happen1);
-            unsigned char *t = res;
+            double *t = res;
             res = mem;
             mem = t;
         }
 
         if (changes_happen2) {
             multiply(mem, in_mem, in_mem, dim, &changes_happen2);
-            unsigned char *t = in_mem;
+            double *t = in_mem;
             in_mem = mem;
             mem = t;
         }
@@ -124,8 +103,7 @@ unsigned char *pow_matrix(unsigned char *in_mem, int dim, int power, int size) {
     // Fill under diagonal part of matrix
     for (int i = 1; i < dim; i++) {
         for (int j = 0; j < i; j++) {
-            memcpy(res + (i * dim + j) * sizeof(double), res + (j * dim + i) * sizeof(double), 
-                    sizeof(double));
+            memcpy(res + (i * dim + j), res + (j * dim + i), sizeof(double));
         }
     }
     munmap(in_mem, size);
@@ -158,7 +136,7 @@ int main(int argc, char const *argv[]) {
 
     // dimension
     int dim = sqrt(size / sizeof(double));
-    
+
     errno = 0;
     char *endptr;
     int power = strtol(argv[3], &endptr, NUMERICAL_SYSTEM_BASE);
@@ -166,13 +144,13 @@ int main(int argc, char const *argv[]) {
         return 0;
     }
 
-    unsigned char *in_mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, in_fd, 0);
-    unsigned char *out_mem = mmap(NULL, size, PROT_WRITE, MAP_SHARED, out_fd, 0);
+    double *in_mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, in_fd, 0);
+    double *out_mem = mmap(NULL, size, PROT_WRITE, MAP_SHARED, out_fd, 0);
     if (in_mem == MAP_FAILED || out_mem == MAP_FAILED) {
         return 1;
     }
 
-    unsigned char *mem = pow_matrix(in_mem, dim, power, size);
+    double *mem = pow_matrix(in_mem, dim, power, size);
     if (mem == NULL) {
         munmap(out_mem, size);
     }
